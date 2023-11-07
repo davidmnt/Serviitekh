a2ba745563974d84dc55f05f4e72575432cdf586
davidmnt <david.montejano@hotmail.com>
2023-11-07 10:22:45 +0100

    m

--- app/src/main/java/com/example/serviitekh/HomeActivity.java
+++ app/src/main/java/com/example/serviitekh/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.serviitekh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageButton botonMarketing = findViewById(R.id.producto1);
+        ImageButton botonRedes = findViewById(R.id.producto2);
+        ImageButton botonWebPredeterminado = findViewById(R.id.producto3);
+        ImageButton botonWebPersonalizadas = findViewById(R.id.producto4);
+
     }
 }
